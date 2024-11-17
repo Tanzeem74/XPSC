81141c8dd6c0c609af9f0a3c6b0e9ea87408f2f3
Tanzeem74 <tanzeemafsar74@gmail.com>
2024-11-17 23:20:43 +0600

    Add files via upload
    added week 4 day 1

---
+++ Week 4/Day 1/RCB vs CSK.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int rcb,csk; cin >> rcb >> csk;
+    if(rcb-csk>=18) cout << "RCB" << endl;
+    else cout << "CSK" << endl;
+
+}
